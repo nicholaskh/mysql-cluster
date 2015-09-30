f678dabfa9c21c619c952ea86db2b46c85548c2d
Nicholas Zhang <zhangkaihong3@wanda.cn>
2015-09-30 10:52:22 +0800

    by default compile the linux env

--- build.sh
+++ build.sh
@@ -10,12 +10,12 @@ ID=$(git rev-parse HEAD | cut -c1-7)
 
 cd daemon/mysql-cluster
 
-if [[ $1 = "-linux" ]]; then
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X github.com/nicholaskh/golib/server.VERSION $VER -X github.com/nicholaskh/golib/server.BuildID $ID -w"
-    mv mysql-cluster ../../bin/mysql-cluster.linux
+if [[ $1 = "-mac" ]]; then
+    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X github.com/nicholaskh/golib/server.VERSION $VER -X github.com/nicholaskh/golib/server.BuildID $ID -w"
+    mv mysql-cluster ../../bin/mysql-cluster.mac
 else
     go build -ldflags "-X github.com/nicholaskh/golib/server.VERSION $VER -X github.com/nicholaskh/golib/server.BuildID $ID -w"
     #go build -race -v -ldflags "-X github.com/nicholaskh/golib/server.BuildID $ID -w"
-    mv mysql-cluster ../../bin/mysql-cluster.mac
-    ../../bin/mysql-cluster.mac -v
+    mv mysql-cluster ../../bin/mysql-cluster.linux
+    ../../bin/mysql-cluster.linux -v
 fi
